e3b9528b806730a26f61f8dbf9774599da4f6440
Hagström Fredrik <hagstrf1@cantor.org.aalto.fi>
2022-06-28 14:23:36 +0300

    Completed and tested implementation of VPGBuffer class in core.py

--- spinup/algos/pytorch/my_vpg/core.py
+++ spinup/algos/pytorch/my_vpg/core.py
@@ -1,3 +1,4 @@
+#from importlib.resources import path
 import torch
 import torch.nn as nn
 
@@ -6,25 +7,122 @@ import numpy as np
 
 class VPGBuffer:
 
-    def __init__(self, size, obs_dim, act_dim, gamma):
+    ### TODO:
+    # Document the VPGBuffer class and its functions
+
+    def __init__(self, size, obs_dim, act_dim, gamma=0.99, device='cpu'):
         self.obs_buf = np.repeat(np.zeros(obs_dim, dtype=np.float32)[None, :], size, axis=0)
         self.act_buf = np.repeat(np.zeros(act_dim, dtype=np.float32)[None, :], size, axis=0)
         self.rew_buf = np.zeros(size, dtype=np.float32)
+        self.val_buf = np.zeros(size, dtype=np.float32)
+
+        self.rtg_buf = np.zeros(size, dtype=np.float32)
         self.adv_buf = np.zeros(size, dtype=np.float32)
 
         self.gamma = gamma
 
-    ###TODO
-    # 1. Check that obs_buf and act_buf are initialized correctly
-    # 2. Implement store function for storing steps in the buffer
-    # 3. Implement get function for getting the data stored in the buffer
+        self.device = device
 
-    def store():
-        pass
+        self.curr_step, self.path_start, self.max_size = 0, 0, size
 
+
+    def store(self, obs, act, rew, val):
+
+        assert self.curr_step < self.max_size
+
+        self.obs_buf[self.curr_step] = obs
+        self.act_buf[self.curr_step] = act
+        self.rew_buf[self.curr_step] = rew
+        self.val_buf[self.curr_step]= val
+
+        self.curr_step += 1
+
+
+    def finish_path(self, last_val):
+        
+        path_idx = slice(self.path_start, self.curr_step)
+        path_vals = np.append(self.val_buf[path_idx], last_val)
+        path_rews = self.rew_buf[path_idx]
+
+        self.adv_buf[path_idx] = path_rews + self.gamma * path_vals[1:] + path_vals[:-1]
+        self.rtg_buf[path_idx] = np.cumsum(path_rews[::-1])[::-1]
+
+        # Optional TODO: Implement infinite-horizon discounted rewards-to-go. Not necessary, the finite-horizon undiscounted
+        # version, i.e., only counting observed rewards on the trajectory, is fine as well. 
+        # path_rews = np.append(self.rew_buf[path_idx], last_val)
+        
+        self.path_start = self.curr_step
     
-    def get():
-        pass
+    def get(self):
+
+        assert self.curr_step == self.max_size
+
+        # Normalize advantages 
+        adv_mean, adv_std = self.adv_buf.mean(), self.adv_buf.std(ddof=1)
+        self.adv_buf = (self.adv_buf - adv_mean) / adv_std
+
+        data = dict(obs=self.obs_buf, act=self.act_buf, 
+                    rew=self.rew_buf, val=self.val_buf, 
+                    rtg=self.rtg_buf, adv=self.adv_buf)
+
+        self.curr_step, self.path_start = 0, 0
+
+        return {k : torch.as_tensor(v, dtype=torch.float32, device=self.device) for k,v in data.items()}
+
+
+def test_buffer(device='cpu'):
+    size = 32
+    obs_dim = (10, 3)
+    act_dim = 5
+
+    buf = VPGBuffer(size, obs_dim, act_dim, device=device)
+
+    for i in range(size):
+        o = np.random.random_sample(obs_dim)
+        a = np.random.random_sample(act_dim)
+        r = np.random.rand()
+        v = np.random.rand()
+
+        buf.store(o, a, r, v)
+    
+    last_val = 0
+    buf.finish_path(last_val)
+
+    data = buf.get()
+    #obs, act, rew, val, rtg, adv = data['obs'], data['act'], data['rew'], data['val'], data['rtg'], data['adv']
+
+    # Test that we can fill the buffer again during next epoch
+    for i in range(size):
+        o = np.random.random_sample(obs_dim)
+        a = np.random.random_sample(act_dim)
+        r = np.random.rand()
+        v = np.random.rand()
+
+        buf.store(o, a, r, v)
+
+    last_val = 0
+    buf.finish_path(last_val)
+
+    data = buf.get()
+    obs, act, rew, val, rtg, adv = data['obs'], data['act'], data['rew'], data['val'], data['rtg'], data['adv']
+
+    print()
+    print("Check shapes of different buffers:")
+    assert obs.shape == (32, 10, 3)
+    print(obs.shape)
+    assert act.shape == (32, 5)
+    print(act.shape)
+    assert rew.shape == (32, )
+    print(rew.shape)
+    print(val.shape)
+    print(rtg.shape)
+    print(adv.shape)
+    print()
+
+    # Check advantage normalization
+    print("Check that advantages are properly normalized:")
+    #print(torch.mean(adv), torch.std(adv), "\n")
+    print(adv.mean(), adv.std(), "\n")
 
 
 def mlp(sizes, activation, output_activation=nn.Identity):
@@ -54,16 +152,31 @@ def mlp(sizes, activation, output_activation=nn.Identity):
 
 
 class MLPActor(nn.Module):
-    def __init__(self, obs_dim, hidden_sizes, act_dim, activation, device='cpu'):
-        super().__init__()
+    pass
+    #def __init__(self, obs_dim, hidden_sizes, act_dim, activation, device='cpu'):
+    #    super().__init__()
 
-        self.obs_dim = obs_dim
-        self.hidden_sizes = hidden_sizes
-        self.act_dim = act_dim
-        self.device = device
+    #    self.obs_dim = obs_dim
+    #    self.hidden_sizes = hidden_sizes
+    #    self.act_dim = act_dim
+    #    self.device = device
 
 
 
 
 class MLPCritic(nn.Module):
     pass
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cuda', action='store_true')
+    args = parser.parse_args()
+    
+    if args.cuda and torch.cuda.is_available():
+        device = 'cuda:0'
+    else:
+        device = 'cpu'
+
+    test_buffer(device)
--- spinup/algos/pytorch/my_vpg/my_vpg.py
+++ spinup/algos/pytorch/my_vpg/my_vpg.py
@@ -1,4 +1,25 @@
 import torch
+from utils.logx import EpochLogger
+
+def my_vpg(env_fn, actor_critic, ac_kwargs=dict(), seed=0, 
+           steps_per_epoch=4000, epochs=50, gamma=0.99, 
+           pi_lr=0.0003, vf_lr=0.001, train_v_iters=80,
+           max_ep_len=1000, logger_kwargs=dict(), save_freq=10):
+
+    
+    logger = EpochLogger(**logger_kwargs)
+    logger.save_config(locals())
+    
+    
+    
+    
+    return
+
+
+# TODO implement observation normalization to see if it makes a difference
+
+
+
 
 if __name__ == '__main__':
     import argparse
@@ -9,6 +30,6 @@ if __name__ == '__main__':
 
 
     if args.cuda and torch.cuda.is_available():
-        device = 'gpu:0'
+        device = 'cuda:0'
     else:
         device = 'cpu'
