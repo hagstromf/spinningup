2fbfc7e2122d53656665b70b030827097ebda8ba
Hagström Fredrik <hagstrf1@cantor.org.aalto.fi>
2022-07-12 16:55:58 +0300

    Added multiprocessing to my_vpg and added ReLU activation to experiment bash scripts

--- spinup/scripts/spinup_vpg_exp.sh
+++ spinup/scripts/spinup_vpg_exp.sh
@@ -6,5 +6,5 @@ declare -a envs=("CartPole-v1" "MountainCarContinuous-v0" "Pendulum-v0" "LunarLa
 
 for env in "${envs[@]}"
 do 
-	python -m spinup.run vpg --env $env --data_dir ~/spinningup/data/vpg_exp/$env --hid\[hid\] \[128,128,128\] --seed 0 10 20 --epochs 150 --dt --act\[act\] torch.nn.Tanh
+	python -m spinup.run vpg --env $env --data_dir ~/spinningup/data/vpg_exp/$env --hid\[hid\] \[128,128,128\] --seed 0 10 20 --epochs 200 --dt --act\[act\] torch.nn.ReLU torch.nn.Tanh --num_cpu 4
 done
